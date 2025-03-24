3e81b90be9a7b6e001d6a639ce3553510ab0256e
bbantier <65014410+bbantier@users.noreply.github.com>
2025-03-24 17:06:48 +0100

    remove unnecessary tests in main.js

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
 import { Tree } from "./bst.js";
 import { prettyPrint } from "./prettyPrint.js";
 
-// Example with an array that short and already sorted
-const simpleArray = [1, 2, 3, 4, 5, 6, 7];
-const simpleTree = new Tree(simpleArray)
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
 
-simpleTree.insert(9);
-simpleTree.insert(10);
+tree.insert(2);
+tree.insert(7453);
+tree.insert(28);
 
-// prettyPrint(simpleTree.root);
+// prettyPrint(tree.root);
 
+tree.deleteItem(23);
 
-// Example with an array that has duplicates and is unsorted
-const complexArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const complexTree = new Tree(complexArray);
+prettyPrint(tree.root);
 
-complexTree.insert(2);
-complexTree.insert(7453);
-complexTree.insert(28);
-
-// prettyPrint(complexTree.root);
-
-complexTree.deleteItem(23);
-
-prettyPrint(complexTree.root);
-
-console.log(complexTree.find(7));
-console.log(complexTree.find(67));
+console.log(tree.find(7));
+console.log(tree.find(67));
