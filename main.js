7b1b7a8660d4970e4c7cba311bb2e01d69ea3bf0
bbantier <65014410+bbantier@users.noreply.github.com>
2025-03-27 12:18:07 +0100

    write testing in main.js

--- main.js
+++ main.js
@@ -1,2 +1,50 @@
 import { Tree } from "./bst.js";
 import { prettyPrint } from "./prettyPrint.js";
+
+const randomArray = Array.from(
+  { length: Math.floor(Math.random() * (30 - 5 + 1)) + 5 },
+  (v, i) => Math.floor(Math.random() * 100) + 1
+);
+
+const tree = new Tree(randomArray);
+
+prettyPrint(tree.root);
+console.log(`The tree is ${tree.isBalanced() ? "" : "not "}balanced.`);
+
+console.log("# Level Order:");
+tree.levelOrder((node) => console.log(node.data));
+
+console.log("# Pre Order:");
+tree.preOrder((node) => console.log(node.data));
+
+console.log("# Post Order:");
+tree.postOrder((node) => console.log(node.data));
+
+console.log("# In Order:");
+tree.inOrder((node) => console.log(node.data));
+
+tree.insert(101);
+tree.insert(102);
+tree.insert(103);
+tree.insert(104);
+tree.insert(105);
+
+prettyPrint(tree.root);
+console.log(`The tree is ${tree.isBalanced() ? "" : "not "}balanced.`);
+
+tree.rebalance();
+
+prettyPrint(tree.root);
+console.log(`The tree is ${tree.isBalanced() ? "" : "not "}balanced.`);
+
+console.log("# Level Order:");
+tree.levelOrder((node) => console.log(node.data));
+
+console.log("# Pre Order:");
+tree.preOrder((node) => console.log(node.data));
+
+console.log("# Post Order:");
+tree.postOrder((node) => console.log(node.data));
+
+console.log("# In Order:");
+tree.inOrder((node) => console.log(node.data));
